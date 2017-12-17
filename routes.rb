8fa19bb28d2a3743374402581b7851c017238271
SkiboPavlo <skibo@i.ua>
2017-12-17 14:23:27 +0200

    create project, db and login

---
+++ routes.rb
@@ -0,0 +1,26 @@
+require "sinatra"
+
+current_dir = Dir.pwd
+Dir["#{current_dir}/models/*.rb"].each { |file| require file }
+
+get '/users' do
+  @users = User.all
+end
+
+
+get '/users/:id' do
+  @user = User.find(params[:id])
+end
+
+post '/users' do
+  @user = User.create(params[:user])
+end
+
+put '/users/:id/publish' do
+  @user = user.find(params[:id])
+  @user.publish!
+end
+
+delete '/users/:id' do
+  user.destroy(params[:id])
+end
